aaa9ae3602643d11030242b67322c473f5b940cc
Ananya <ananyaagrawal073@gmail.com>
2023-11-20 11:48:46 +0530

    commit

---
+++ 01-Fundamentals-Part-1/02_statements and commands/first.js
@@ -0,0 +1,84 @@
+var a;
+a = 5;
+// this will popup file
+//alert(a);
+var digit1 = 10;
+var digit2 = 20;
+console.log(digit1);
+// var has removed from the es6
+// let is used to declare the variable
+const x = 2;
+let y = 'hi'
+console.log(y)
+// const cannot be chnaged
+let gn;
+gn = 10;
+gn = 20;
+console.log(gn);
+
+// Data types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Object
+// 7. Symbol
+// 8. BigInt
+/*
+
+//operators
+// 1. Arithmetic operators
+// 2. Assignment operators
+// 3. Comparison operators
+// 4. Logical operators
+// 5. Bitwise operators
+// 6. String operators
+// 7. Conditional (ternary) operator
+// 8. Comma operator
+// 9. Unary operators
+// 10. Relational operators
+*/
+
+let yu;
+yu = 90;
+let ui = 87;
+console.log(yu + ui);
+console.log(yu - ui);
+console.log(yu * ui);
+console.log(yu / ui);
+console.log(yu % ui);
+console.log(yu ** ui);
+console.log(yu++);
+console.log(yu--);
+console.log(++yu);
+console.log(--yu);
+console.log(yu += ui);
+console.log(yu -= ui);
+console.log(yu *= ui);
+console.log(yu /= ui);
+console.log(yu %= ui);
+console.log(yu **= ui);
+console.log(yu &= ui);
+console.log(yu |= ui);
+console.log(yu ^= ui);
+console.log(yu >>= ui);
+console.log(yu <<= ui);
+console.log(yu >>>= ui);
+console.log(yu == ui);
+console.log(yu === ui);
+console.log(yu != ui);
+console.log(yu !== ui);
+console.log(yu > ui);
+console.log(yu < ui);
+console.log(yu >= ui);
+console.log(yu <= ui);
+console.log(yu && ui);
+console.log(yu || ui);
+console.log(!yu);
+console.log(yu & ui);
+console.log(yu | ui);
+console.log(yu ^ ui);
+console.log(~yu);
+console.log(yu << ui);
+
